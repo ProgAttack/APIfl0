d67498b89b8a83408be29c1ede519705d41b5b7b
Giancarlo Kramer <92767138+ProgAttack@users.noreply.github.com>
2023-12-13 09:31:46 -0300

    Add files via upload

---
+++ Models/ColaboradorComValorTotal.cs
@@ -0,0 +1,10 @@
+﻿namespace InfobarAPI.Models
+{
+    public class ColaboradorComValorTotal
+
+    {
+        public int IdCol { get; set; }
+        public string Nome { get; set; }
+        public double ValorTotal { get; set; }
+    }
+}
